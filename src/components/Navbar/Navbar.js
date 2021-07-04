e49769d007e342203078625128980c4879e3ee17
Ramos Felipe <felipeofor@gmail.com>
2021-07-04 11:37:24 -0300

    Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import react from 'react'
+import './Navbar.scss'
+
+export default function Navbar(){
+    return(
+        <div id="header">
+			<nav>
+				<ul class="nav">
+					<li><a href="">Inicio</a></li>
+					<li><a href="">Servicios</a>
+						<ul>
+							<li><a href="">Submenu1</a></li>
+							<li><a href="">Submenu2</a></li>
+							<li><a href="">Submenu3</a></li>
+							<li><a href="">Submenu4</a>
+								<ul>
+									<li><a href="">Submenu1</a></li>
+									<li><a href="">Submenu2</a></li>
+									<li><a href="">Submenu3</a></li>
+									<li><a href="">Submenu4</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li><a href="">Acerca de</a>
+						<ul>
+							<li><a href="">Submenu1</a></li>
+							<li><a href="">Submenu2</a></li>
+							<li><a href="">Submenu3</a></li>
+							<li><a href="">Submenu4</a></li>
+						</ul>
+					</li>
+					<li><a href="">Contacto</a></li>
+					<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00b341" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="6" cy="19" r="2" />
+  <circle cx="17" cy="19" r="2" />
+  <path d="M17 17h-11v-14h-2" />
+  <path d="M6 5l14 1l-1 7h-13" />
+</svg>
+				</ul>
+			</nav>
+		</div>
+    )
+}
