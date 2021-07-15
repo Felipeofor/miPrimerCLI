c67b54d8f05a1481be890a4eb22a0b8c03f6e32f
Ramos Felipe <felipeofor@gmail.com>
2021-07-15 19:43:21 -0300

    calibrando item

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,33 +1,65 @@
-//Importamos los componentes
-import react from 'react'
-import './Item.scss';
-import React, { useRef } from "react";
-import ReactDom from "react-dom";
+import React, { useState , useEffect} from "react";
+import "./Item.scss";
 
+export default function App() {
+  
+  const [myItem, setMyItem] = useState({})
 
-export const Item = ({ setShowModal, elements}) => {
-  console.log(elements)
-  // Cerrramos el modal cuando hacemos click fuera de el
-  const modalRef = useRef();
-  const closeModal = (e) => {
-    if (e.target === modalRef.current) {
-      setShowModal(false);
-    }
-  };
-
-
- const productos = fetch('../../public/productos');
-
-  setTimeout(() => {
-    
-}, 2000);
+  useEffect(()=>{
+    fetch('./productos.json')
+    .then((itemMl)=> { 
+         let itemFinal= itemMl.json() 
+         return itemFinal
+        })
+      .then((itemJson)=>{
+        setMyItem(itemJson)
+      })
+  },[])
   
 
-  /*Imagen y caracteristicas del pokemon dentro del modal */
-  return ReactDom.createPortal(
-    <div className="container" ref={modalRef} onClick={closeModal}>
-      <Item />
-    </div>
+  console.log("myItem-->",myItem )
+
+  /*Contador*/
+
+const [counter,setCounter] = useState(0);
+const stock = 5;
+function agregar(){
+  if(counter < stock){
+    setCounter(counter+1)
+  }
+}
+
+function quitar(){
+  if(counter > 0){
+    setCounter(counter-1)
+  }
+}
+  
+  
+  return (
+    <div className="Cards">
+           <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+                <div className="card h-100 mx-3">
+                <img src={myItem.pictureUrl} class="card-img-top" alt="Producto n°1"/>
+                <div className="card-body">
+                    <h5 className="card-title">{myItem.title}</h5>
+                    <p className="card-text">{myItem.description}</p>
+                </div>
+                <div className="card-footer">{myItem.description}
+                <div>
+                <h2 className={counter}>{counter}</h2>
+                <div className="botones" >
+                    <button onClick={quitar} >-</button>
+                    <button onClick={agregar} >+</button>
+                    
+                </div><button className="caracteristicas-btn" 
+            >Ver caracteristicas</button>
+                </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
   );
-};
-export default Item;
+}
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,63 +1,14 @@
-import react, { useEffect } from 'react';
+import react from 'react';
 import './ItemList.scss';
-import { useState } from 'react';
-import  Item  from '../Item';
+import Item from '../Item';
 
 
-
-
-export default function ItemList (props) {
-
-        /*Item*/
-
-        const [showModal, setShowModal] = useState(false);
-        const openModal = (props) => {
-          setShowModal(true, props);
-        };
-
-        /*Contador*/
-
-    const [counter,setCounter] = useState(0);
-    const stock = 5;
-    function agregar(){
-        if(counter < stock){
-          setCounter(counter+1)
-        }
-      }
-
-    function quitar(){
-        if(counter > 0){
-          setCounter(counter-1)
-        }
-      }
+export default function ItemListContainer (props) {
 
 
     return (
-        <div className="Cards">
-           <div className="row row-cols-1 row-cols-md-3 g-4">
-            <div className="col">
-                <div className="card h-100 mx-3">
-                <img src="https://jumboargentina.vteximg.com.br/arquivos/ids/614663-1000-1000/Aceite-De-Girasol-Cocinero-900-Ml-1-32670.jpg?v=637409202878630000" class="card-img-top" alt="Producto n°1"/>
-                <div className="card-body">
-                    <h5 className="card-title"></h5>
-                    <p className="card-text"></p>
-                </div>
-                <div className="card-footer">
-                <div>
-                <h2 className={counter}>{counter}</h2>
-                <div className="botones" >
-                    <button onClick={quitar} >-</button>
-                    <button onClick={agregar} >+</button>
-                    
-                </div><button className="caracteristicas-btn" onClick={openModal} 
-            >Ver caracteristicas</button>
-            {showModal ? <Item setShowModal={setShowModal}
-            /> : null}
-                </div>
-                </div>
-                </div>
-            </div>
-            </div>
+        <div>
+             <Item />
         </div>
     )
 }
