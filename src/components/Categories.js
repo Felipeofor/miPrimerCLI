1afa14f153a1438bd168aeb0695aa45d44bc0d1d
Ramos Felipe <felipeofor@gmail.com>
2021-07-27 18:02:23 -0300

    correccion 1.2

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,9 +3,9 @@ import React from 'react'
  * ademas de poder reutilizarlo multiples veses el componente
  * podemos usar el Hook useParams
  *  */
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 
-import {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Categories() {
@@ -27,11 +27,10 @@ export default function Categories() {
 
     const getCategory = async () => {
         // Espero a que la llamda se fetchee
-        const llamada = await fetch('../JSON/Datalist.json');
+        const llamada = await fetch('../../JSON/Datalist.json');
         // Aplico el método JSON() para extraer la respuesta a la petición
         const responsellamada = await llamada.json()
         // Vemos qué llegó
-
         setProductsCategory(responsellamada.filter((product) => product.categoria === categoryByID));
     }
 
@@ -44,8 +43,9 @@ export default function Categories() {
             <h1>Categoria {categoryByID}</h1>
             {productsCategory.map(element =>{
                 return (
-                        <Link to={`/ItemDetailConteiner/${element.id}`}><p> {element.title}</p>
-                        </Link>
+
+                        // <Link to={`/ItemDetailConteiner/${element.id}`}><p> {element.title}</p>
+                        // </Link>
                 )
             })}
         </div>
