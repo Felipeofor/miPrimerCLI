862cd1ac7e37b354491d583b5032589a8de99515
Ramos Felipe <felipeofor@gmail.com>
2021-08-05 15:20:38 -0300

    carrito correccion 1.0

--- src/Context.js
+++ src/Context.js
@@ -4,13 +4,28 @@ export const useCartContext = () => useContext(CartContext);
 
 const CartProvider = ({children}) =>{
     const [cartCount, setCartCount] = useState(0);
+    const [cartItems, setCartItems] = useState([]);
 
-    const updateCartCount = qty =>{
-        setCartCount(prev => prev + qty)
-    };
+    const addToCart = (item, qty) =>{
+        if(cartItems.some(product = product.title === item.title)){
+            const copy = [...cartItems]
+            const repeteadIndex = cartItems.findeIndex (product => product.title === item.title);
+            copy[repeteadIndex] = {
+                ...copy[repeteadIndex],
+                count: copy[repeteadIndex].qty + qty
+            };
+            setCartItems(copy)
+        }
+
+        else{
+         setCartItems([...cartItems,{...item, qty }]);   
+        }
+
+        setCartCount(prev => prev + qty);
+    }
 
     return(
-        <CartContext.Provider value= {{ cartCount, updateCartCount }}>{children}</CartContext.Provider>
+        <CartContext.Provider value= {{ cartCount, cartItems , addToCart }}>{children}</CartContext.Provider>
     )
 };
 
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import { useCartContext } from '../Context'
+
 function Cart() {
+    const { cartItems } = useCartContext()
+    console.log(CartItems);
     return(
         <>
         Soy el carrito!
+        {cartItems.length > 0 && cartItems.map((item, index) =>{
+            <p key={index}>{item.title} x {item.qty}</p>
+        })}
         </>
     )
 }
