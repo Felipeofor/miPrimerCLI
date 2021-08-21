ff9772169d012fd4b1b14d657ff6cd9cd0707ce0
Ramos Felipe <felipeofor@gmail.com>
2021-08-21 14:24:59 -0300

    [cart] removeItems and total()

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useCartContext } from '../Context';
+import { Link } from "react-router-dom"
 
 function Cart() {
   const {items, total, removeItems} = useCartContext();
@@ -21,10 +22,13 @@ function Cart() {
             )
           })
         ) : (
+          <div className="Cart__vacia">
           <h3 className="Cart__h3">El carrito esta vacio :(</h3>
+          <Link ClassName="Cart__link" to="/">Volver a Home</Link>
+          </div>
         )
       }
-      <p className="Cart__total">Total: {total()}</p>
+      {items.length > 0 ? (<p className="Cart__total">Total: {total()}</p>) : (<></>)}
     </div>
   )
 }
